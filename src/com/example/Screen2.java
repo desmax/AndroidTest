b52283d6a5a02783fecd1c14ca8b65fb7bb4be72
Maxim Pustynnikov <pustinnikov@gmail.com>
2012-06-17 16:13:23 +0300

    Test application

---
+++ src/com/example/Screen2.java
@@ -0,0 +1,11 @@
+package com.example;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: maxim
+ * Date: 6/17/12
+ * Time: 3:38 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Screen2 {
+}
